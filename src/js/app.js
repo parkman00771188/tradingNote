c2ada87935040880117520e19008f296c10a89fc
parkman00771188 <parkman00771188@users.noreply.github.com>
2026-06-25 15:45:47 +0900

    Use GIS token login on all devices

--- src/js/app.js
+++ src/js/app.js
@@ -3493,12 +3493,6 @@ document.addEventListener("keydown", (event) => {
 });
 
 window.addEventListener("hashchange", () => {
-  if (typeof processGoogleRedirectLoginResponse === "function" && processGoogleRedirectLoginResponse()) {
-    render();
-    scrollPageToTop();
-    return;
-  }
-
   cancelActiveModalDraft();
   activeModal = null;
   assetCashError = "";
@@ -3522,7 +3516,4 @@ window.visualViewport?.addEventListener("resize", scheduleMobileViewportInset, {
 window.visualViewport?.addEventListener("scroll", scheduleMobileViewportInset, { passive: true });
 initializeUserDataState();
 updateMobileViewportInset();
-if (typeof processGoogleRedirectLoginResponse === "function") {
-  processGoogleRedirectLoginResponse();
-}
 render();
